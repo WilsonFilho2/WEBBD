d37356bad2b8fa82ba4495e7d47fb12be9df0a67
WilsonFilho2 <wilson.menezes09@aluno.ifce.edu.br>
2023-03-23 16:13:25 -0300

    Informações dos voos

--- flights/views.py
+++ flights/views.py
@@ -8,3 +8,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": models.Flight.objects.all(),
     })
+
+
+def flight(request, flight_id):
+    flight = models.Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
