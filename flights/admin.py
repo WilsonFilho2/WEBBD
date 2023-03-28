9e756c553093d0e3261378fdc796c93fa49cbfd8
WilsonFilho2 <wilson.menezes09@aluno.ifce.edu.br>
2023-03-28 15:57:37 -0300

    book

--- flights/admin.py
+++ flights/admin.py
@@ -3,6 +3,10 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Flight)
 admin.site.register(models.Airport)
 admin.site.register(models.Passenger)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id","origin","destination","duration")
+
+admin.site.register(models.Flight, FlightAdmin)
--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import request
+from django.http import request, HttpResponseRedirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
@@ -13,8 +14,18 @@ def index(request):
 def flight(request, flight_id):
     flight = models.Flight.objects.get(id=flight_id)
     passengers = flight.passengers.all()
+    non_passengers = models.Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers
+        "passengers": passengers,
+        "non_passengers": non_passengers
     })
 
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = models.Flight.objects.get(id=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = models.Passenger.objects.get(id=passenger_id)
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
